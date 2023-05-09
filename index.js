040facefe0b04fdffcb8928dcaadb57d0e066821
Thanu Poptiphueng <tpcrasite@gmail.com>
2023-05-09 13:07:27 +0700

    added validation

--- index.js
+++ index.js
@@ -4,14 +4,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const zod_1 = require("zod");
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 const port = 3000;
+const schema = zod_1.z.object({
+    name: zod_1.z.string(),
+    lastName: zod_1.z.string(),
+});
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 app.post("/", (req, res) => {
-    res.send(`Hello ${req.body.name}!`);
+    const input = schema.parse(req.body);
+    res.send(`Hello ${input.name} + ${input.lastName}!`);
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- index.ts
+++ index.ts
@@ -1,14 +1,21 @@
 import express from "express";
+import { z } from "zod";
 const app = express();
 app.use(express.json());
 const port = 3000;
+
+const schema = z.object({
+  name: z.string(),
+  lastName: z.string(),
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.post("/", (req, res) => {
-  res.send(`Hello ${req.body.name}!`);
+  const input = schema.parse(req.body);
+  res.send(`Hello ${input.name} + ${input.lastName}!`);
 });
 
 app.listen(port, () => {
